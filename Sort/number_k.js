6b0b0a90e48efd5b66f8059c1ef680e964481a48
appleStory20 <py.hotz2019@gmail.com>
2020-04-13 22:49:22 +0900

    update

---
+++ Sort/number_k.js
@@ -0,0 +1,9 @@
+
+function solution(array, commands) {
+    var answer = [];
+
+    answer = commands.map(a=>{
+        return array.slice(a[0]-1,a[1]).sort((b,c)=>b-c)[a[2]-1];
+    })
+    return answer;
+}
